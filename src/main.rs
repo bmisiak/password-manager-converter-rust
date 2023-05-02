72c88709ed0d37a5e4d365874593cf8e135cfc7e
Brian M <bmisiak@me.com>
2023-05-02 16:37:33 -0700

    more pretty with itertools

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::{bail, Context, Result};
+use itertools::Itertools;
 use std::env::args;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 use crate::sinks::strongbox_csv::Strongbox;
 use crate::sources::enpass_json::EnpassJson;
@@ -15,56 +16,42 @@ mod universal;
 fn main() -> Result<()> {
     let mut args = args();
     let invocation = args.next().unwrap();
-    let (source_name, source_reader, sink_name, sink_destination) = match (
-        args.next(),
-        args.next(),
-        args.next().as_deref(),
-        args.next(),
-        args.next(),
-    ) {
-        (Some(source_name), Some(source_path), Some("to"), Some(sink_name), Some(sink_path)) => {
-            let source_reader: Box<dyn BufRead> = if source_name == "stdin" {
-                Box::new(BufReader::new(std::io::stdin()))
-            } else {
-                Box::new(BufReader::new(
-                    File::open(&source_path)
-                        .with_context(|| format!("Unable to read {source_path}"))?,
-                ))
-            };
+    if let Some((source_name, source_path, _to, sink_name, sink_path)) = args.next_tuple() {
+        let source_reader: Box<dyn BufRead> = if source_name == "stdin" {
+            Box::new(BufReader::new(stdin()))
+        } else {
+            Box::new(BufReader::new(
+                File::open(&source_path).context(format!("Unable to read {source_path}"))?,
+            ))
+        };
 
-            let sink_destination = File::options()
-                .write(true)
-                .truncate(true)
-                .create(true)
-                .open(&sink_path)
-                .with_context(|| format!("Unable to open {sink_path}"))?;
+        let sink_destination = File::options()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(&sink_path)
+            .context(format!("Unable to open {sink_path}"))?;
 
-            (source_name, source_reader, sink_name, sink_destination)
-        }
-        _ => {
-            println!("Usage: {invocation} enpass SOURCE_FILE to strongbox OUTPUT_FILE");
-            println!("Sample usage: {invocation} enpass ./export.json to strongbox ./out.csv");
-            return Ok(());
-        }
-    };
+        let source: Box<dyn Source> = match source_name.as_str() {
+            "enpass" => Box::new(
+                EnpassJson::from_reader(source_reader)
+                    .context(format!("Unable to parse the enpass .json export"))?,
+            ),
+            _ => bail!("Unsupported source name"),
+        };
 
-    let source: Box<dyn Source> = match source_name.as_str() {
-        "enpass" => Box::new(
-            EnpassJson::from_reader(source_reader)
-                .with_context(|| format!("Unable to parse the enpass .json export"))?,
-        ),
-        _ => bail!("Unsupported source name"),
-    };
+        let mut sink: Box<dyn Sink> = match sink_name.as_str() {
+            "strongbox" => Box::new(Strongbox::with_output(sink_destination)),
+            _ => bail!("Unsupported target name"),
+        };
 
-    let mut sink: Box<dyn Sink> = match sink_name.as_str() {
-        "strongbox" => Box::new(Strongbox::with_output(sink_destination)),
-        _ => bail!("Unsupported target name"),
-    };
+        sink.convert_from_source(source)
+            .context(format!("Unable to convert to {sink_name}"))?;
 
-    sink.convert_from_source(source)
-        .with_context(|| format!("Unable to convert to {sink_name}"))?;
-
-    println!("Conversion successful!");
-
+        println!("Conversion successful!");
+    } else {
+        println!("Usage: {invocation} enpass SOURCE_FILE to strongbox OUTPUT_FILE");
+        println!("Sample usage: {invocation} enpass ./export.json to strongbox ./out.csv");
+    }
     Ok(())
 }
